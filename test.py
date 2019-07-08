ac5cef8cfa19a2e8393d7ab0b016be2e14d56d01
Arij Trabelsi <52330025+ArijTrabelsi@users.noreply.github.com>
2019-07-08 15:23:31 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+age =input('Enter your age')
+age =int(age)
+if age<18:
+    print('you are too young')
+elif age<=22:
+    print('welcome to Egypt scolarship program')
+else:
+    print('you are too old')
